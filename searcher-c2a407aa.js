99f97299046f0622e83d8b542ac5341f169c91ec
yang <yang@MacBook-Air-5.local>
2026-02-12 14:53:34 +0900

    new

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f5b42ed2.js',
+                path_to_root + 'searchindex-9218460c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
